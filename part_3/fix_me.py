ca985d4ffd2a6185fe523bd1e71496af6e168caa
Natalia Rivera R <nzr0037@auburn.edu>
2020-02-21 22:02:03 -0600

    Last changes of the script

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -60,12 +60,12 @@ print("Now \"b\" equals", b, "and is a", type(b))
 
 
 total = a + b
- print("a + b =", total, ", which is a", type(total))
+print("a + b =", total, ", which is a", type(total))
 
 difference = a - b
 print("a - b =", difference, ", which is a", type(difference))
 
-    product = a * b
+product = a * b
 print("a * b =", product, ", which is a", type(product))
 
 quotient = a / b
@@ -75,14 +75,10 @@ floor_quotient = a // b
 print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
 
 remainder = a % b
-print("a % b =," remainder ", which is a," type(remainder))
+print("a % b =,", remainder, ", which is a", type(remainder))
 
 power = a ** b
-print("a ** b =",
-        power,
-        ", which is a",
-        type(power),
-        )
+print("a ** b =", power,", which is a", type(power))
 
 a += 1
 print("Incrementing \"a\" by one results in", a, "which is a", type(a))
