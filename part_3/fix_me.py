1fb2108a68aef12079ffdcf30ca6e84399d93fc4
Natalia Rivera R <nzr0037@auburn.edu>
2020-02-25 00:28:58 -0600

    Right shebang line

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,24 +1,23 @@
-#!/bin/python3
+#! /bin/python3
 
 # Reading data from the user's input
-a=input("Enter a : ")
+a = input("Enter a : ")
 
-print("You entered",a,", which is a", type(a))
+print("You entered", a, "which is a", type(a))
 
-b=input("Enter b : ")
+b = input("Enter b : ")
 
-print("You entered",b,", which is a", type(b))
+print("You entered", b, "which is a", type(b))
 
 
 #######################################################################
 # HINT: why would we be checking what type a and b are again?
 #       Let's assume we want a and b to be integers at this point
 #######################################################################
-a=int(a)
-b=int(b)
 
 print("What is", a, "now?", type(a))
 print("What is", b, "now?", type(a))
+
 
 total = a + b
 print("a + b =", total, ", which is a", type(total))
@@ -60,12 +59,12 @@ print("Now \"b\" equals", b, "and is a", type(b))
 
 
 total = a + b
-print("a + b =", total, ", which is a", type(total))
+ print("a + b =", total, ", which is a", type(total))
 
 difference = a - b
 print("a - b =", difference, ", which is a", type(difference))
 
-product = a * b
+    product = a * b
 print("a * b =", product, ", which is a", type(product))
 
 quotient = a / b
@@ -75,10 +74,14 @@ floor_quotient = a // b
 print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
 
 remainder = a % b
-print("a % b =,", remainder, ", which is a", type(remainder))
+print("a % b =," remainder ", which is a," type(remainder))
 
 power = a ** b
-print("a ** b =", power,", which is a", type(power))
+print("a ** b =",
+        power,
+        ", which is a",
+        type(power),
+        )
 
 a += 1
 print("Incrementing \"a\" by one results in", a, "which is a", type(a))
